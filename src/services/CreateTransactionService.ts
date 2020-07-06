441bf16f60e2ac065207047453487c32e3afe432
Gabriel Maquiavelli <gabrielmaquiavelli@gmail.com>
2020-07-06 18:47:24 -0300

    Challenge completed with 100% approved tests.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: RequestDTO): Transaction {
     // TODO
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw new Error('Insufficient balance!');
+    }
+
+    const newTransaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return newTransaction;
   }
 }
 
